fa1000d5ee559269f8abdc7f8b24a6b4f76116ff
정영일 <bbangwon@outlook.com>
2024-05-26 21:15:00 +0900

    최종 스타일 지정

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import CssBaseline from '@mui/material/CssBaseline'
 import TodoList from './TodoList'
+import Navbar from './Navbar'
 
 import './App.css'
 
 function App() {
   return (
     <>     
+      <Navbar />
       {/* CssBaseline은 기본적인 CSS를 초기화해주는 컴포넌트입니다. */}
       <CssBaseline />
-      <h1>할일목록</h1>
       <TodoList />
     </>
   )
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,11 +2,13 @@ import { useState, useEffect } from 'react';
 import List from '@mui/material/List';
 import TodoItem from './TodoItem';
 import TodoForm from './TodoForm';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 const getInitialData = () => {
     const data = localStorage.getItem('todos');
     return data ? JSON.parse(data) : [];
-} 
+}
 
 export default function TodoList() {
     const [todos, setTodos] = useState(getInitialData);
@@ -30,22 +32,33 @@ export default function TodoList() {
     const addTodo = (text) => {
         console.log(text);
         setTodos(
-            [...todos, { id: crypto.randomUUID(), text, completed: false}]
+            [...todos, { id: crypto.randomUUID(), text, completed: false }]
         );
-};
+    };
 
-return (
-    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-        {todos.map(todo => (
-            <TodoItem
-                todo={todo}
-                key={todo.id}
-                remove={removeTodo}
-                toggle={() => toggleTodo(todo.id)} />
-        ))}
-        <TodoForm addTodo={addTodo} />
-    </List>
-);
+    return (
+        <Box sx={{
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            alignItems: "center",
+            m: 3
+            }}>
+        <Typography variant="h2" component="div" sx={{ flexGrow: 1 }}>
+            할일 목록
+          </Typography>
+            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                {todos.map(todo => (
+                    <TodoItem
+                        todo={todo}
+                        key={todo.id}
+                        remove={removeTodo}
+                        toggle={() => toggleTodo(todo.id)} />
+                ))}
+                <TodoForm addTodo={addTodo} />
+            </List>
+        </Box>
+    );
 }
 
 
